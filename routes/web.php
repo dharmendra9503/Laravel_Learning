70857e495f809e4d2444c9b77e94c32a9e0f4a8f
DHARMENDRA PRAJAPATI <dharmendraprajapati9503@gmail.com>
2024-08-08 11:09:25 +0530

    Remove duplicates by delare in class and use that using class name.

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,45 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-$jobs = [
-    [
-        "id" => 1,
-        "title" => "Director",
-        "salary" => "$50,0000"
-    ],
-    [
-        "id" => 2,
-        "title" => "Programmer",
-        "salary" => "$10,000"
-    ],
-    [
-        "id" => 3,
-        "title" => "Teacher",
-        "salary" => "$40,000"
-    ]
-];
+// $jobs = [
+//     [
+//         "id" => 1,
+//         "title" => "Director",
+//         "salary" => "$50,0000"
+//     ],
+//     [
+//         "id" => 2,
+//         "title" => "Programmer",
+//         "salary" => "$10,000"
+//     ],
+//     [
+//         "id" => 3,
+//         "title" => "Teacher",
+//         "salary" => "$40,000"
+//     ]
+// ];
+
+class Job {
+    public static function all() {
+        return [
+            [
+                "id" => 1,
+                "title" => "Director",
+                "salary" => "$50,0000"
+            ],
+            [
+                "id" => 2,
+                "title" => "Programmer",
+                "salary" => "$10,000"
+            ],
+            [
+                "id" => 3,
+                "title" => "Teacher",
+                "salary" => "$40,000"
+            ]
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home');
@@ -33,21 +55,21 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs', function () use ($jobs) {
+Route::get('/jobs', function () {
     return view('jobs', [
-        "jobs" => $jobs
+        "jobs" => Job::all()
     ]);
 });
 
-ROute::get('/jobs/{id}', function ($id) use ($jobs) {
+ROute::get('/jobs/{id}', function ($id) {
     /*
      * In above method we don't have access of external variable so to use that external variable we need to use "use" keyword
      */
-    // $job = Arr::first($jobs, function ($job) use ($id) {
+    // $job = Arr::first(Job::all(), function ($job) use ($id) {
     //     return $job['id'] == $id;
     // });
 
-    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+    $job = Arr::first(Job::all(), fn ($job) => $job['id'] == $id);
 
     return view('job', ['job' => $job]);
 });
